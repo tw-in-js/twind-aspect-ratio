2c7d8022a79419b97f75fb632a3c58b5331e2532
Sascha Tandel <s.tandel@kenoxa.de>
2021-01-29 16:48:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# @twind/aspect-ratio
+
+A plugin for [Twind](https://twind.dev) that provides a composable API for giving elements a fixed aspect ratio.
+
+[![MIT License](https://flat.badgen.net/github/license/tw-in-js/twind-aspect-ratio)](https://github.com/tw-in-js/twind-aspect-ratio/blob/main/LICENSE)
+[![Latest Release](https://flat.badgen.net/npm/v/@twind/aspect-ratio?icon=npm&label&cache=10800&color=blue)](https://www.npmjs.com/package/@twind/aspect-ratio)
+[![Github](https://flat.badgen.net/badge/icon/tw-in-js%2Ftwind-aspect-ratio?icon=github&label)](https://github.com/tw-in-js/twind-aspect-ratio)
+[![Module Size](https://flat.badgen.net/badgesize/brotli/https:/unpkg.com/@twind/aspect-ratio/aspect-ratio.js?icon=jsdelivr&label&color=blue&cache=10800)](https://unpkg.com/@twind/aspect-ratio/aspect-ratio.js 'brotli module size')
+[![Typescript](https://flat.badgen.net/badge/icon/included?icon=typescript&label)](https://unpkg.com/browse/@twind/aspect-ratio/aspect-ratio.d.ts)
+
+> Based on [@tailwindcss/aspect-ratio](https://github.com/tailwindlabs/tailwindcss-aspect-ratio).
+
+## Installation
+
+Install from npm:
+
+```sh
+# Using npm
+npm install @twind/aspect-ratio
+
+# Using Yarn
+yarn add @twind/aspect-ratio
+```
+
+## Usage as Directive
+
+Note that due to the way this currently needs to be implemented ([the old padding-bottom trick](https://css-tricks.com/aspect-ratio-boxes/)) you need to assign the aspect ratio to a _parent_ element, and make the actual element you are trying to size the only child of that parent.
+
+Once the [`aspect-ratio` property](https://developer.mozilla.org/en-US/docs/Web/CSS/aspect-ratio) is supported in modern browsers, we'll add official support to Twind itself and deprecate this plugin.
+
+Use the `aspectRatio` function to specify the aspect ratio for an element:
+
+```js
+import { aspectRatio } from '@twind/aspect-ratio'
+
+document.body.innerHTML = `
+  <div class="${tw(aspectRatio(16, 9))}">
+    <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+`
+
+// Alternative APIs
+aspectRatio('16/9')
+aspectRatio({ w: 16, h: 9 })
+```
+
+To remove any aspect ratio, use `aspectRatio('none')`:
+
+```js
+document.body.innerHTML = `
+  <div class="${tw`${aspectRatio(16, 9)} lg:${aspectRatio('none')}`}">
+    <!-- ... -->
+  </div>
+`
+```
+
+## Usage as Plugin
+
+Add the plugin to plugins of your setup call:
+
+```js
+import { aspectRatio } from '@twind/aspect-ratio'
+
+setup({
+  plugins: {
+    'aspect': aspectRatio,
+  },
+})
+```
+
+Combine the `aspect-w-{n}` and `aspect-h-{n}` classes to specify the aspect ratio for an element:
+
+```html
+<div class="aspect-w-16 aspect-h-9">
+  <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+```
+
+Use `aspect-x/y` shorthand:
+
+```html
+<div class="aspect-16/9">
+  <!-- ... -->
+</div>
+```
+
+Use `aspect-x-y` shorthand:
+
+```html
+<div class="aspect-16-9">
+  <!-- ... -->
+</div>
+```
+
+Use `aspect-none` to remove any aspect ratio behavior:
+
+```html
+<div class="aspect-w-16 aspect-h-9 lg:aspect-none">
+  <!-- ... -->
+</div>
+```
+
+## License
+
+[MIT](https://github.com/tw-in-js/aspect-ratio/blob/main/LICENSE)
---
+++ test.ts
@@ -0,0 +1,111 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+
+import type { Instance } from 'twind'
+import type { VirtualSheet } from 'twind/sheets'
+
+import { create } from 'twind'
+import { virtualSheet } from 'twind/sheets'
+import { aspectRatio } from '.'
+
+const test = suite<{
+  sheet: VirtualSheet
+  tw: Instance['tw']
+}>('@twind/aspect-ratio')
+
+test.before((context) => {
+  context.sheet = virtualSheet()
+  const { tw } = create({
+    sheet: context.sheet,
+    mode: 'strict',
+    preflight: false,
+    prefix: false,
+    plugins: {
+      aspect: aspectRatio,
+    },
+  })
+  context.tw = tw
+})
+
+test.after.each(({ sheet }) => {
+  sheet.reset()
+})
+
+test('using directive', ({ tw, sheet }) => {
+  assert.is(tw(aspectRatio(16, 9)), 'tw-1sfq0fj aspect-ratio')
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.tw-1sfq0fj{--tw-aspect-w:16;--tw-aspect-h:9}',
+  ])
+
+  sheet.reset()
+
+  assert.is(tw(aspectRatio('none')), 'tw-lqatbl')
+  assert.equal(sheet.target, [
+    '.tw-lqatbl>*{position:static;height:auto;width:auto;top:auto;right:auto;bottom:auto;left:auto}',
+    '.tw-lqatbl{position:static;padding-bottom:0px}',
+  ])
+
+  sheet.reset()
+
+  assert.is(tw(aspectRatio('1.85/1')), 'tw-mikpg4 aspect-ratio')
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.tw-mikpg4{--tw-aspect-w:1.85;--tw-aspect-h:1}',
+  ])
+
+  sheet.reset()
+
+  assert.is(tw(aspectRatio({ w: 3, h: 2 })), 'tw-gy87d0 aspect-ratio')
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.tw-gy87d0{--tw-aspect-w:3;--tw-aspect-h:2}',
+  ])
+
+  sheet.reset()
+
+  assert.is(tw(aspectRatio(4, 3)), 'tw-vvqhtr aspect-ratio')
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.tw-vvqhtr{--tw-aspect-w:4;--tw-aspect-h:3}',
+  ])
+})
+
+test('using plugin', ({ tw, sheet }) => {
+  assert.is(
+    tw`aspect-w-16 aspect-h-9 lg:aspect-none`,
+    'aspect-w-16 aspect-h-9 aspect-ratio lg:aspect-none',
+  )
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.aspect-w-16{--tw-aspect-w:16}',
+    '.aspect-h-9{--tw-aspect-h:9}',
+    '@media (min-width:1024px){.lg\\:aspect-none>*{position:static;height:auto;width:auto;top:auto;right:auto;bottom:auto;left:auto}}',
+    '@media (min-width:1024px){.lg\\:aspect-none{position:static;padding-bottom:0px}}',
+  ])
+
+  sheet.reset()
+
+  assert.is(tw`aspect-4/3`, 'aspect-4/3 aspect-ratio')
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.aspect-4\\/3{--tw-aspect-w:4;--tw-aspect-h:3}',
+  ])
+
+  sheet.reset()
+
+  assert.is(tw`aspect-1.85-1`, 'aspect-1.85-1 aspect-ratio')
+  assert.equal(sheet.target, [
+    '.aspect-ratio>*{position:absolute;height:100%;width:100%;top:0px;right:0px;bottom:0px;left:0px}',
+    '.aspect-ratio{position:relative;padding-bottom:calc(var(--tw-aspect-h)/var(--tw-aspect-w)*100%)}',
+    '.aspect-1\\.85-1{--tw-aspect-w:1.85;--tw-aspect-h:1}',
+  ])
+})
+
+test.run()
